7965ac180500cac57c6925ddbf45d495cea76271
Thabetbelaid <thabet94@hotmail.com>
2022-06-16 13:12:00 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,10 @@
 
 use Andyabih\LaravelToUML\Http\Controllers\LaravelToUMLController;
 use Illuminate\Support\Facades\Route;
+// This will work and generate everything properly.
+use App\Models\Post;
 
-Route::get(config('laravel-to-uml.route'), [LaravelToUMLController::class, 'index']);
+// Using it directly in the code without the use operator won't.
+$posts = \App\Models\Post::all();
+
+Route::get(config ('laravel-to-uml.route'), [LaravelToUMLController::class, 'index']);
